95274924500fcfcc8bae028039844737037558e5
Krish Patel <krishpkreame1405+github@gmail.com>
2025-09-21 04:47:16 +1200

    Update AuthContext.jsx

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 
 const AuthContext = createContext();
 
+// Eslint directive to disable the warning about hooks in non-component functions
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
