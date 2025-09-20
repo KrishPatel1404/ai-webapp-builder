43fb7a4696f51eef0aff3e22d89ab82c08fdbd27
Krish Patel <krishpkreame1405+github@gmail.com>
2025-09-21 04:03:53 +1200

    Add React Router and update entry point
    Integrated React Router into main.jsx to support routing between components. Updated index.html to reference Main.jsx instead of main.jsx for consistency with file naming.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './style.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home.jsx";
+import Login from "./Login.jsx";
+import "./style.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
