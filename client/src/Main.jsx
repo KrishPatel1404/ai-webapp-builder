404688b584e3f9bbf35356eb979e50831cb4b349
Krish Patel <krishpkreame1405+github@gmail.com>
2025-09-25 10:38:07 +1200

    Update Main.jsx

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import Home from "./Home.jsx";
+import About from "./About.jsx";
 import Login from "./Login.jsx";
 import Profile from "./Profile.jsx";
 import Requirements from "./Requirements.jsx";
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")).render(
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/requirements" element={<Requirements />} />
