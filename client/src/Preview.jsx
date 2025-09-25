d3980b483884db1b0b6f4673768d2776580f8243
Krish Patel <krishpkreame1405+github@gmail.com>
2025-09-26 11:22:51 +1200

    Update Preview.jsx

--- client/src/Preview.jsx
+++ client/src/Preview.jsx
@@ -114,7 +114,7 @@ function Preview() {
                   ${
                     isMobile
                       ? `
-                    transform: scale(0.7);
+                    transform: scale(0.75);
                     transform-origin: top left;
                     width: 133.33%;
                     height: 133.33%;
